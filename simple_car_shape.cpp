8dcf1530cc3c5f45697ed18cd2bb323029b71d3f
Bimal Kunwar <bimalknwr3@gmail.com>
2025-01-08 19:16:06 +0545

    Update simple_car_shape.cpp

--- simple_car_shape.cpp
+++ simple_car_shape.cpp
@@ -2,27 +2,28 @@
 #include <stdio.h>
 
 void drawCar() {
-    setcolor(WHITE);
+
+    setcolor(BLUE);
     rectangle(150, 300, 450, 400); 
-    setfillstyle(SOLID_FILL, WHITE);
-    floodfill(151, 301, WHITE); 
+    setfillstyle(SOLID_FILL, BLUE);
+    floodfill(151, 301, BLUE); 
 
-
+   
     setcolor(GREEN);
     line(200, 300, 400, 300);    
-    line(200, 300, 250, 250);    
-    line(400, 300, 350, 250);    
-    line(250, 250, 350, 250);    
+    line(200, 300, 250, 250);   
+    line(400, 300, 350, 250);   
+    line(250, 250, 350, 250);   
     setfillstyle(SOLID_FILL, GREEN);
-    floodfill(251, 251, GREEN);  
+    floodfill(251, 251, GREEN); 
 
     setcolor(BLACK);
-    circle(200, 420, 20); 
+    circle(200, 420, 20);
     setfillstyle(SOLID_FILL, BLACK);
-    floodfill(200, 420, BLACK); 
+    floodfill(200, 420, BLACK);
 
-    
-    circle(400, 420, 20); 
+   
+    circle(400, 420, 20);
     floodfill(400, 420, BLACK); 
 }
 
@@ -33,7 +34,7 @@ int main() {
 
     drawCar(); 
 
-    getch(); 
+    getch();
     closegraph(); 
     return 0;
 }
